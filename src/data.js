2694945b7d2e3ca0e030ee912f055e57d32153f8
Yanquiel Arango Gomez <yanquielwebdesigner@gmail.com>
2021-02-26 14:17:34 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,73 @@
+
+export default{
+  
+  products: [
+    {
+      _id: '1',
+      name: 'Full Zip Running Shirts',
+      category: 'Shirts',
+      image: '/images/product-1.jpg',
+      price: 60,
+      brand: 'Hiskywin',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Nike Full-Zip Hoodie Mens Workout Sweatshirt',
+      category: 'Shirts',
+      image: '/images/product-2.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 5,
+      numReviews: 51,
+      
+    },
+    {
+      _id: '3',
+      name: 'Adidas Mens Long Sleeve Hoodie',
+      category: 'Shirts',
+      image: '/images/product-3.jpg',
+      price: 55,
+      brand: 'Adidas',
+      rating: 4,
+      numReviews: 45,
+      
+    },
+    {
+      _id: '4',
+      name: 'Under Armour Mens Sportstyle Tricot Joggers',
+      category: 'Pants',
+      image: '/images/product-4.jpg',
+      price: 90,
+      brand: 'Under Armour',
+      rating: 2.5,
+      numReviews: 198,
+      countInStock: 6,
+    },
+    {
+      _id: '5',
+      name: 'Champion Mens Graphic Powerblend fleece Jogger',
+      category: 'Pants',
+      image: '/images/product-5.jpg',
+      price: 60,
+      brand: 'Champion',
+      rating: 4.5,
+      numReviews: 86,
+      countInStock: 6,
+    },
+    {
+      _id: '6',
+      name: 'Aelfric Eden Mens Joggers Pants Long Multi-Pockets',
+      category: 'Shirts',
+      image: '/images/product-6.jpg',
+      price: 60,
+      brand: 'Aelfric Eden',
+      rating: 4.5,
+      numReviews: 45,
+      countInStock: 6,
+    },
+  ],
+};
+
